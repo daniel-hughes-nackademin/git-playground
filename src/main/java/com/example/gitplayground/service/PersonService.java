9f0541aa4f4e9f3e625b389b369733ce9a83e50c
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2021-03-16 22:51:47 +0100

    feat: Add person service REPP(1234)

---
+++ src/main/java/com/example/gitplayground/service/PersonService.java
@@ -0,0 +1,29 @@
+package com.example.gitplayground.service;
+
+import com.example.gitplayground.model.Person;
+import com.example.gitplayground.repository.PersonRepository;
+import org.springframework.data.redis.support.atomic.RedisAtomicInteger;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PersonService {
+
+    private final RedisAtomicInteger personIdCounter;
+    private final PersonRepository personRepository;
+
+    public PersonService(RedisAtomicInteger personIdCounter, PersonRepository personRepository) {
+        this.personIdCounter = personIdCounter;
+        this.personRepository = personRepository;
+    }
+
+
+    public Person createPerson(String name){
+        Person person = new Person(personIdCounter.getAndIncrement(), name)
+    }
+
+    public Person findById(int id){
+        return personRepository.findById(id)
+                .orElseThrow();
+    }
+
+}
